16d29ac332b6474cd03a4fdf4ffbb087a0fdc59c
danielclamor <danielclamor@gmail.com>
2025-06-20 16:45:47 -0600

    feat: Add navigation to recipe_view

--- lib/components/molecules/recipe_card.dart
+++ lib/components/molecules/recipe_card.dart
@@ -1,4 +1,6 @@
 import 'package:elchemist_app/models/recipe.dart';
+import 'package:elchemist_app/transitions.dart';
+import 'package:elchemist_app/views/recipe_view.dart';
 import 'package:flutter/material.dart';
 
 class RecipeCard extends StatefulWidget {
@@ -14,40 +16,67 @@ class _RecipeCardState extends State<RecipeCard> {
   @override
   Widget build(BuildContext context) {
     final recipe = widget.recipe;
-    return Card(
-      margin: EdgeInsets.zero,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(
-          4.0,
+    return InkWell(
+      child: Card(
+        margin: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            4.0,
+          ),
+        ),
+        color: Colors.white,
+        surfaceTintColor: Colors.transparent,
+        elevation: 1,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 8.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                recipe.name,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "${recipe.brand} - ${recipe.nicType.name.toUpperCase()}",
+                style: const TextStyle(
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      color: Colors.white,
-      surfaceTintColor: Colors.transparent,
-      elevation: 1,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16.0,
-          vertical: 8.0,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              recipe.name,
-              style: const TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
-              ),
+      onTap: () {
+        Navigator.push(
+          context,
+          PageRouteBuilder(
+            pageBuilder: (
+              context,
+              animation,
+              secondaryAnimation,
+            ) =>
+                RecipeView(recipe: recipe),
+            transitionsBuilder: (
+              context,
+              animation,
+              secondaryAnimation,
+              child,
+            ) =>
+                slideTransitionBuilder(
+              context,
+              animation,
+              secondaryAnimation,
+              child,
             ),
-            Text(
-              "${recipe.brand} - ${recipe.nicType.name.toUpperCase()}",
-              style: const TextStyle(
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
