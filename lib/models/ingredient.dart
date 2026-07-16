b1821c6ca45da10ca00cefa8ca1887144ed7ae2f
danielclamor <danielclamor@gmail.com>
2026-07-16 14:55:01 -0600

    add: Add percentage field and ingredient type

--- lib/models/ingredient.dart
+++ lib/models/ingredient.dart
@@ -1,11 +1,37 @@
+enum IngredientType {
+  nicotine,
+  vg,
+  pg,
+  vgFlavor,
+  pgFlavor;
+}
+
+IngredientType getIngredientType(String ingredientType) {
+  if (ingredientType == "nicotine") {
+    return IngredientType.nicotine;
+  } else if (ingredientType == "vg") {
+    return IngredientType.vg;
+  } else if (ingredientType == "pg") {
+    return IngredientType.pg;
+  } else if (ingredientType == "vgFlavor") {
+    return IngredientType.vgFlavor;
+  } else {
+    return IngredientType.pgFlavor;
+  }
+}
+
 class Ingredient {
   final String name;
-  final double volume;
-  final double weight;
+  double percentage;
+  double volume;
+  double weight;
+  final IngredientType type;
 
   Ingredient({
     required this.name,
+    required this.percentage,
     required this.volume,
     required this.weight,
+    required this.type,
   });
 }
