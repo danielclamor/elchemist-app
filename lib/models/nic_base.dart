9f4835d3f05c1392c51d39991ef2cb2457c21c30
danielclamor <danielclamor@gmail.com>
2026-07-20 15:43:28 -0600

    change isVg to isVG

--- lib/models/nic_base.dart
+++ lib/models/nic_base.dart
@@ -1,20 +1,20 @@
 class NicBase {
   final String code;
   final String name;
-  final bool isVg;
+  final bool isVG;
   final double percentage;
 
   NicBase({
     required this.code,
     required this.name,
-    required this.isVg,
+    required this.isVG,
     required this.percentage,
   });
 
   factory NicBase.fromMap(Map<String, dynamic> map) => NicBase(
         code: map["code"] as String,
         name: map["name"] as String,
-        isVg: map["is_vg"] as bool,
+        isVG: map["is_vg"] as bool,
         percentage: map["percentage"] as double,
       );
 }
