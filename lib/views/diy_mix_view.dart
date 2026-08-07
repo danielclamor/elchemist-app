8f9ca56d6264cb8e68450be8734a00ff66e04063
danielclamor <danielclamor@gmail.com>
2026-08-07 11:46:57 -0600

    implement section organisms

--- lib/views/diy_mix_view.dart
+++ lib/views/diy_mix_view.dart
@@ -1,6 +1,9 @@
-import 'package:elchemist_app/components/atoms/el_text_field.dart';
 import 'package:elchemist_app/components/molecules/flavoring_entry_row.dart';
+import 'package:elchemist_app/components/organisms/batch_section.dart';
+import 'package:elchemist_app/components/organisms/flavoring_section.dart';
+import 'package:elchemist_app/components/organisms/nic_base_section.dart';
 import 'package:elchemist_app/components/organisms/recipe_section.dart';
+import 'package:elchemist_app/components/organisms/target_section.dart';
 import 'package:elchemist_app/providers/mix_recipe_calculator.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -56,7 +59,7 @@ class _DiyMixViewState extends State<DiyMixView> {
   }
 
   void _addFlavoringEntry() {
-    final flavor = 'Flavor ${_flavoringEntries.length + 1}';
+    final flavor = 'Flavour ${_flavoringEntries.length + 1}';
     const ratio = 0.0;
 
     final entry = FlavoringEntry(
@@ -73,18 +76,6 @@ class _DiyMixViewState extends State<DiyMixView> {
       entry.dispose();
       _flavoringEntries.remove(entry);
     });
-  }
-
-  Widget _buildEntryRow(FlavoringEntry entry, bool withHeaders) {
-    return FlavoringEntryRow(
-      entry: entry,
-      withHeaders: withHeaders,
-      showDeleteIcon: true,
-      onEntryDeleted: () => _removeFlavoringEntry(entry),
-      onNameSubmitted: (value) => setState(() {}),
-      onPercentSubmitted: (value) => setState(() {}),
-      onIsVGChanged: (value) => setState(() {}),
-    );
   }
 
   @override
@@ -124,244 +115,83 @@ class _DiyMixViewState extends State<DiyMixView> {
                     Column(
                       spacing: 12.0,
                       children: [
-                        Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(4.0),
-                          ),
-                          margin: EdgeInsets.zero,
-                          child: Container(
-                            padding: const EdgeInsets.all(16.0),
-                            constraints: const BoxConstraints(
-                              maxWidth: 500,
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  "BATCH",
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                const Gap(16.0),
-                                ElTextField(
-                                  controller: _volumeController,
-                                  contentType: ElTextFieldContentType.numeric,
-                                  labelText: 'Volume',
-                                  labelPosition: ElTextFieldLabelPosition.left,
-                                  suffixText: 'mL',
-                                  onSubmitted: (value) => setState(() {}),
-                                ),
-                              ],
-                            ),
-                          ),
+                        BatchSection(
+                          width: sectionWidth,
+                          volumeController: _volumeController,
+                          onVolumeSubmitted: (value) => setState(() {}),
                         ),
-                        Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(4.0),
-                          ),
-                          margin: EdgeInsets.zero,
-                          child: Container(
-                            padding: const EdgeInsets.all(16.0),
-                            constraints: const BoxConstraints(
-                              maxWidth: 500,
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  "FLAVOURING",
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                const Gap(20),
-                                Column(
-                                  spacing: 8.0,
-                                  children: List.generate(
-                                      _flavoringEntries.length, (index) {
-                                    final withHeaders =
-                                        index == 0 ? true : false;
+                        FlavoringSection(
+                          width: sectionWidth,
+                          flavoringEntries: List.generate(
+                            _flavoringEntries.length,
+                            (index) {
+                              final entry = _flavoringEntries[index];
+                              final withHeaders = index == 0 ? true : false;
 
-                                    return _buildEntryRow(
-                                      _flavoringEntries[index],
-                                      withHeaders,
-                                    );
-                                  }),
+                              return FlavoringEntryRow(
+                                entry: entry,
+                                withHeaders: withHeaders,
+                                showDeleteIcon: true,
+                                onEntryDeleted: () =>
+                                    _removeFlavoringEntry(entry),
+                                onNameSubmitted: (value) => setState(() {}),
+                                onPercentSubmitted: (value) => setState(() {}),
+                                onIsVGChanged: (value) => setState(() {}),
+                              );
+                            },
+                          ),
+                          addEntryButton: Padding(
+                            padding: const EdgeInsets.only(top: 8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                TextButton(
+                                  onPressed: () => _addFlavoringEntry(),
+                                  child: const Text("+ Add"),
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 8.0),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      TextButton(
-                                        onPressed: () => _addFlavoringEntry(),
-                                        child: const Text("+ Add"),
-                                      ),
-                                    ],
-                                  ),
-                                )
                               ],
                             ),
                           ),
                         ),
                       ],
                     ),
-                    Container(
-                      constraints: BoxConstraints(
-                        maxWidth: midSectionWidth,
-                      ),
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4.0),
-                        ),
-                        margin: EdgeInsets.zero,
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                "NIC BASE",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const Gap(20),
-                              ElTextField(
-                                controller: _nicBaseNicStrController,
-                                contentType: ElTextFieldContentType.numeric,
-                                labelText: 'Nic Str',
-                                labelPosition: ElTextFieldLabelPosition.left,
-                                onSubmitted: (value) => setState(() {}),
-                                suffixText: '%',
-                              ),
-                              const Gap(8),
-                              Row(
-                                spacing: 8.0,
-                                children: [
-                                  Expanded(
-                                    child: ElTextField(
-                                      controller: _nicBaseVGController,
-                                      contentType:
-                                          ElTextFieldContentType.numeric,
-                                      labelText: 'VG',
-                                      labelPosition:
-                                          ElTextFieldLabelPosition.left,
-                                      suffixText: '%',
-                                      onSubmitted: (value) {
-                                        setState(() {
-                                          _nicBasePGController.text =
-                                              (100 - (double.parse(value)))
-                                                  .toString();
-                                        });
-                                      },
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: ElTextField(
-                                      controller: _nicBasePGController,
-                                      contentType:
-                                          ElTextFieldContentType.numeric,
-                                      labelText: 'PG',
-                                      labelPosition:
-                                          ElTextFieldLabelPosition.left,
-                                      suffixText: '%',
-                                      onSubmitted: (value) {
-                                        setState(() {
-                                          _nicBaseVGController.text =
-                                              (100 - (double.parse(value)))
-                                                  .toString();
-                                        });
-                                      },
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
+                    NicBaseSection(
+                      width: midSectionWidth,
+                      nicStrController: _nicBaseNicStrController,
+                      onNicStrSubmitted: (value) => setState(() {}),
+                      vgController: _nicBaseVGController,
+                      onVGSubmitted: (value) {
+                        setState(() {
+                          _nicBasePGController.text =
+                              (100 - (double.parse(value))).toString();
+                        });
+                      },
+                      pgController: _nicBasePGController,
+                      onPGSubmitted: (value) {
+                        setState(() {
+                          _nicBaseVGController.text =
+                              (100 - (double.parse(value))).toString();
+                        });
+                      },
                     ),
-                    Container(
-                      constraints: BoxConstraints(
-                        maxWidth: midSectionWidth,
-                      ),
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4.0),
-                        ),
-                        margin: EdgeInsets.zero,
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                "TARGET",
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const Gap(20),
-                              ElTextField(
-                                controller: _targetNicStrController,
-                                contentType: ElTextFieldContentType.numeric,
-                                labelText: 'Nic Str',
-                                labelPosition: ElTextFieldLabelPosition.left,
-                                suffixText: '%',
-                                onSubmitted: (value) => setState(() {}),
-                              ),
-                              const Gap(8.0),
-                              Row(
-                                spacing: 8.0,
-                                children: [
-                                  Expanded(
-                                    child: ElTextField(
-                                      controller: _targetVGController,
-                                      contentType:
-                                          ElTextFieldContentType.numeric,
-                                      labelText: 'VG',
-                                      labelPosition:
-                                          ElTextFieldLabelPosition.left,
-                                      suffixText: '%',
-                                      onSubmitted: (value) {
-                                        setState(() {
-                                          _targetPGController.text =
-                                              (100 - (double.parse(value)))
-                                                  .toString();
-                                        });
-                                      },
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: ElTextField(
-                                      controller: _targetPGController,
-                                      contentType:
-                                          ElTextFieldContentType.numeric,
-                                      labelText: 'PG',
-                                      labelPosition:
-                                          ElTextFieldLabelPosition.left,
-                                      suffixText: '%',
-                                      onSubmitted: (value) {
-                                        setState(() {
-                                          _targetVGController.text =
-                                              (100 - (double.parse(value)))
-                                                  .toString();
-                                        });
-                                      },
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
+                    TargetSection(
+                      width: midSectionWidth,
+                      nicStrController: _targetNicStrController,
+                      onNicStrSubmitted: (value) => setState(() {}),
+                      vgController: _targetVGController,
+                      onVGSubmitted: (value) {
+                        setState(() {
+                          _targetPGController.text =
+                              (100 - (double.parse(value))).toString();
+                        });
+                      },
+                      pgController: _targetPGController,
+                      onPGSubmitted: (value) {
+                        setState(() {
+                          _targetVGController.text =
+                              (100 - (double.parse(value))).toString();
+                        });
+                      },
                     ),
                     RecipeSection(
                       width: sectionWidth,
@@ -377,7 +207,7 @@ class _DiyMixViewState extends State<DiyMixView> {
                             double.parse(_targetNicStrController.text) / 100,
                         targetVG: double.parse(_targetVGController.text) / 100,
                         targetPG: double.parse(_targetPGController.text) / 100,
-                        flavorings: _flavoringEntries,
+                        flavoringEntries: _flavoringEntries,
                       ).ingredients,
                     ),
                   ],
