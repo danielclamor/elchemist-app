871a21e539c41c791900d6a95b59d2a1d3d1c639
danielclamor <danielclamor@gmail.com>
2026-08-07 16:40:46 -0600

    refactor FormulaSection

--- lib/components/organisms/formula_section.dart
+++ lib/components/organisms/formula_section.dart
@@ -12,9 +12,7 @@ import 'package:gap/gap.dart';
 class FormulaSection extends StatelessWidget {
   final double? width;
   final Formula? formula;
-  final SearchController? searchController;
-  final SuggestionsBuilder? suggestionsBuilder;
-  final VoidCallback? onChangeFormula;
+  final Widget? auxiliaryTool;
   final NicProfile? nicProfile;
   final bool isNicProfileFinal;
   final ValueChanged<NicProfile?>? onNicProfileSelected;
@@ -28,9 +26,7 @@ class FormulaSection extends StatelessWidget {
     super.key,
     this.width,
     required this.formula,
-    this.searchController,
-    this.suggestionsBuilder,
-    this.onChangeFormula,
+    this.auxiliaryTool,
     required this.nicProfile,
     bool isNicProfileFinal = false,
     this.onNicProfileSelected,
@@ -46,134 +42,94 @@ class FormulaSection extends StatelessWidget {
     return SectionCard(
       width: width,
       title: "FORMULA",
-      child: formula == null && suggestionsBuilder != null
-          ? SearchAnchor(
-              searchController: searchController,
-              viewShape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(4.0),
-                ),
-              ),
-              viewSide: const BorderSide(
-                color: Colors.white,
-              ),
-              builder: (context, controller) {
-                return SearchBar(
-                  onTap: () {
-                    searchController?.openView();
-                  },
-                  leading: const Icon(Icons.search),
-                  elevation: const WidgetStatePropertyAll(
-                    0.0,
-                  ),
-                  shape: const WidgetStatePropertyAll(
-                    RoundedRectangleBorder(
-                      side: BorderSide(),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(4.0),
-                      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    formula!.brand.toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
                     ),
                   ),
-                );
-              },
-              suggestionsBuilder: suggestionsBuilder!,
-            )
-          : Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          formula!.brand.toUpperCase(),
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        Text(
-                          formula!.name,
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          '${formula!.nicType.toString()} — ${formula!.chilltype.toString()}',
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
+                  Text(
+                    formula!.name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
-                    onChangeFormula == null
-                        ? const SizedBox.shrink()
-                        : IconButton(
-                            icon: const Icon(
-                              Icons.change_circle_sharp,
-                            ),
-                            onPressed: onChangeFormula,
-                          ),
-                  ],
-                ),
-                const Gap(20),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: ElDropdownMenu<NicProfile>(
-                        width: 360,
-                        labelText: 'Profile',
-                        initialSelection: nicProfile,
-                        enabled: true,
-                        dropdownMenuEntries:
-                            UnmodifiableListView<DropdownMenuEntry<NicProfile>>(
-                          formula!.nicProfiles
-                              .map<DropdownMenuEntry<NicProfile>>(
-                            (nicProfile) => DropdownMenuEntry<NicProfile>(
-                              value: nicProfile,
-                              label: nicProfile.label,
-                            ),
-                          ),
-                        ),
-                        onSelected: onNicProfileSelected,
+                  ),
+                  Text(
+                    '${formula!.nicType.toString()} — ${formula!.chilltype.toString()}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+              auxiliaryTool ?? const SizedBox.shrink(),
+            ],
+          ),
+          const Gap(20),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: ElDropdownMenu<NicProfile>(
+                  width: 360,
+                  labelText: 'Profile',
+                  initialSelection: nicProfile,
+                  enabled: true,
+                  dropdownMenuEntries:
+                      UnmodifiableListView<DropdownMenuEntry<NicProfile>>(
+                    formula!.nicProfiles.map<DropdownMenuEntry<NicProfile>>(
+                      (nicProfile) => DropdownMenuEntry<NicProfile>(
+                        value: nicProfile,
+                        label: nicProfile.label,
                       ),
                     ),
-                    SizedBox(
-                      width: 140,
-                      child: Padding(
-                        padding: const EdgeInsetsGeometry.only(
-                          left: 8.0,
-                          right: 12.0,
-                        ),
-                        child: ElTextField(
-                          controller: nicLevelController,
-                          contentType: ElTextFieldContentType.numeric,
-                          readOnly: onNicLevelSubmitted == null,
-                          labelText: 'Nic Level',
-                          suffixText: 'mg',
-                          onSubmitted: onNicLevelSubmitted,
-                        ),
-                      ),
-                    ),
-                    showCustomCheckBox
-                        ? ElCheckbox(
-                            width: 50,
-                            value: isCustom,
-                            labelText: 'Custom',
-                            onChanged: onIsCustomChanged,
-                          )
-                        : const SizedBox.shrink(),
-                  ],
+                  ),
+                  onSelected: onNicProfileSelected,
                 ),
-              ],
-            ),
+              ),
+              SizedBox(
+                width: 140,
+                child: Padding(
+                  padding: const EdgeInsetsGeometry.only(
+                    left: 8.0,
+                    right: 12.0,
+                  ),
+                  child: ElTextField(
+                    controller: nicLevelController,
+                    contentType: ElTextFieldContentType.numeric,
+                    readOnly: onNicLevelSubmitted == null,
+                    labelText: 'Nic Level',
+                    suffixText: 'mg',
+                    onSubmitted: onNicLevelSubmitted,
+                  ),
+                ),
+              ),
+              showCustomCheckBox
+                  ? ElCheckbox(
+                      width: 50,
+                      value: isCustom,
+                      labelText: 'Custom',
+                      onChanged: onIsCustomChanged,
+                    )
+                  : const SizedBox.shrink(),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
