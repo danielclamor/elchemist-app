14ae7aa647f06abaef5becad2b0f07a3d5236370
danielclamor <danielclamor@gmail.com>
2026-08-11 15:53:47 -0600

    add ApiService

---
+++ lib/services/api_service.dart
@@ -0,0 +1,63 @@
+import 'package:elchemist_app/services/api_models.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+final HttpLink _httpLink = HttpLink('http://localhost:8000/graphql');
+
+GraphQLClient get graphQLClient => GraphQLClient(
+      link: _httpLink,
+      cache: GraphQLCache(store: HiveStore()),
+    );
+
+class ApiService {
+  Future<List<FormulaDto>> getFormulas() async {
+    final QueryResult result = await graphQLClient.query(QueryOptions(
+      document: gql(
+        r'''
+query ReadFormulas {
+formulas {
+    slug
+    name
+    brand
+    chillType
+    nicType
+    nicProfiles {
+      slug
+      name
+      targetVg
+      targetPg
+      nicBases {
+        nicBaseOption {
+          code
+          name
+          isVg
+        }
+        ratio
+      }
+      fullName
+      isNewMix
+      nicBaseNicStr
+      targetNicStr
+      flavorings {
+        isVg
+        name
+        ratio
+      }
+    }
+  }
+}
+''',
+      ),
+      fetchPolicy: FetchPolicy.networkOnly,
+    ));
+
+    if (result.hasException) {
+      throw result.exception!;
+    }
+
+    return (result.data?['formulas'] as List<dynamic>?)
+            ?.map((formula) =>
+                FormulaDto.fromJson(Map<String, dynamic>.from(formula as Map)))
+            .toList() ??
+        [];
+  }
+}
